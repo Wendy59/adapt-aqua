ac74f34775dfce93b5ac4d69fb88183b26265047
Wendy <wendynorbert.toradiha@gmail.com>
2019-02-02 17:18:16 +0100

    ADD : Inclus les sections tarifs & horaires (sections vides) DEL : Suppression de la section "action"

---
+++ resources/views/horaires.blade.php
@@ -0,0 +1,11 @@
+<section id="horaires">
+    <div class="container wow fadeIn">
+        <div class="section-header">
+            <h3 class="section-title">Horaires et réservation </h3>
+            <p class="section-description">Horaires & réservation description</p>
+        </div>
+        <div class="row">
+            In Progress
+        </div>
+    </div>
+</section>
