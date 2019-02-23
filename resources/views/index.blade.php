f1b751c07a5babdc29d733960b52b54397b6f2d1
Wendy <wendynorbert.toradiha@gmail.com>
2019-02-23 20:07:59 +0100

    ADD : Ajout d'un calendrier reprenant les horaires pour chaque prestations

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -75,6 +75,218 @@
         $('.calling_modalCoursAqua').on('click', function(){$('#modalCoursAqua').modal('show')});
         $('.calling_modalCoursCollectif').on('click', function(){$('#modalCoursCollectif').modal('show')});
         $('.calling_modalHydromassage').on('click', function(){$('#modalHydromassage').modal('show')});
+        
+        const scheduler_aquatique = [{
+                title: 'Aquatique',
+                dow: [1,2,3,4],
+                start: '09:00',
+                end: '10:00',
+                color: '#45a89e'
+            },{
+                title: 'Aquatique',
+                dow: [5],
+                start: '08:30',
+                end: '09:30',
+                color: '#45a89e'
+            },{
+                title: 'Aquatique',
+                dow: [1,2,3,4],
+                start: '10:00',
+                end: '11:00',
+                color: '#45a89e'
+            },{
+                title: 'Aquatique',
+                dow: [5],
+                start: '09:30',
+                end: '10:30',
+                color: '#45a89e'
+            },{
+                title: 'Aquatique',
+                dow: [1,2,3,4],
+                start: '11:00',
+                end: '12:00',
+                color: '#45a89e'
+            },{
+                title: 'Aquatique',
+                dow: [5],
+                start: '10:30',
+                end: '11:30',
+                color: '#45a89e'
+            },{
+                title: 'Aquatique',
+                dow: [1,2,3],
+                start: '14:00',
+                end: '15:00',
+                color: '#45a89e'
+            },{
+                title: 'Aquatique',
+                dow: [5],
+                start: '14:30',
+                end: '15:30',
+                color: '#45a89e'
+            },{
+                title: 'Aquatique',
+                dow: [2],
+                start: '15:00',
+                end: '16:00',
+                color: '#45a89e'
+            },{
+                title: 'Aquatique',
+                dow: [3,4],
+                start: '17:00',
+                end: '18:00',
+                color: '#45a89e'
+            },{
+                title: 'Aquatique',
+                dow: [1,2,5],
+                start: '17:30',
+                end: '18:30',
+                color: '#45a89e'
+            },{
+                title: 'Aquatique',
+                dow: [3,4],
+                start: '18:00',
+                end: '19:00',
+                color: '#45a89e'
+            },{
+                title: 'Aquatique',
+                dow: [1,2,5],
+                start: '18:30',
+                end: '19:30',
+                color: '#45a89e'
+            },{
+                title: 'Aquatique',
+                dow: [3,4],
+                start: '19:00',
+                end: '20:00',
+                color: '#45a89e'
+            },{
+                title: 'Aquatique',
+                dow: [2],
+                start: '19:30',
+                end: '20:30',
+                color: '#45a89e'
+            }];
+        const scheduler_espace_detente = [{
+                    title: 'Espace détente',
+                    dow: [1,2,3,4,5],
+                    start: '10:00',
+                    end: '11:00',
+                    color: '#dda66e'
+                },{
+                    title: 'Espace détente',
+                    dow: [1,2,3,4,5],
+                    start: '11:00',
+                    end: '12:00',
+                    color: '#dda66e'
+                },{
+                    title: 'Espace détente',
+                    dow: [1,2,3,4,5],
+                    start: '14:00',
+                    end: '15:00',
+                    color: '#dda66e'
+                },{
+                    title: 'Espace détente',
+                    dow: [1,2,3,4,5],
+                    start: '15:00',
+                    end: '16:00',
+                    color: '#dda66e'
+                },{
+                    title: 'Espace détente',
+                    dow: [1,2,3,4,5],
+                    start: '16:00',
+                    end: '17:00',
+                    color: '#dda66e'
+                },{
+                    title: 'Espace détente',
+                    dow: [1,2,3,4,5],
+                    start: '17:00',
+                    end: '18:00',
+                    color: '#dda66e'
+                },{
+                    title: 'Espace détente',
+                    dow: [1,2,3,4,5],
+                    start: '18:00',
+                    end: '19:00',
+                    color: '#dda66e'
+                },{
+                    title: 'Espace détente',
+                    dow: [1,2,3,4,5],
+                    start: '19:00',
+                    end: '20:00',
+                    color: '#dda66e'
+                }];
+        const scheduler_aquaphobie = [{
+                title: 'Cours de natation / Aquaphobie',
+                dow: [1,2,3,4,5],
+                start: '15:00',
+                end: '16:00',
+                color: '#f5a9a9'
+            },{
+                title: 'Cours de natation / Aquaphobie',
+                dow: [1,2,3,4,5],
+                start: '16:00',
+                end: '17:00',
+                color: '#f5a9a9'
+            },{
+                title: 'Cours de natation / Aquaphobie',
+                dow: [1,2,3,4,5],
+                start: '17:00',
+                end: '17:30',
+                color: '#f5a9a9'
+        }];
+        const scheduler_collectif = [];
+        
+        
+        function clearCalendar(){
+            $("#calendar").fullCalendar( 'removeEvents' );
+        };
+        
+        function setEventIntoCalendar(events){
+            $("#calendar").fullCalendar( 'addEventSource', events);
+        };
+        
+        $('#calendar').fullCalendar({
+            header: false,
+            defaultView: 'agendaWeek',
+            columnFormat: 'dddd',
+            allDaySlot: false,
+//            hiddenDays: [0, 6], // on masque samedi, dimanche
+            minTime: '07:00:00',
+            maxTime: '22:00:00',
+            height: 'auto',
+            businessHours: [{
+                start: '08:00',
+                end: '21:00', 
+                dow: [1,2,3,4,5]
+            }],
+            events: scheduler_aquatique
+        });
+        
+        //Affiche les heures de séance aqua
+        $('#scheduler_aquatique').on('click', function(){
+            clearCalendar();
+            setEventIntoCalendar(scheduler_aquatique);
+        });
+        
+        //Affiche les heures de séance espace détente
+        $('#scheduler_espace_detente').on('click', function(){
+            clearCalendar();
+            setEventIntoCalendar(scheduler_espace_detente);
+        });
+        
+        //Affiche les heures de séance aquaphobie et cours de natation
+        $('#scheduler_aquaphobie').on('click', function(){
+            clearCalendar();
+            setEventIntoCalendar(scheduler_aquaphobie);
+        });
+        
+        //Affiche les heures de séance espace détente
+        $('#scheduler_collectif').on('click', function(){
+            clearCalendar();
+            setEventIntoCalendar(scheduler_collectif);
+        });
+        
     </script>
 
 </body>
