174ad5893f80583a53b2a317b5d130b4da78f0a0
Wendy <wendynorbert.toradiha@gmail.com>
2019-02-24 16:33:47 +0100

    ADD : tarifs

--- resources/views/tarifs.blade.php
+++ resources/views/tarifs.blade.php
@@ -2,10 +2,98 @@
     <div class="container wow fadeIn">
         <div class="section-header">
             <h3 class="section-title">Tarifs</h3>
-            <p class="section-description">Tarifs description</p>
         </div>
-        <div class="row">
-            In Progress
-        </div>
+        <div class="row">   
+            <div class="col-lg-4 col-md-6">
+                <div class="box featured">
+                    <h3>ADAPT 1 - 12 mois **</h3>
+                    <h4>39<sup>€</sup><span> / mois</span></h4>
+                    <ul>
+                        <li><i class="fas fa-check"></i> 1 séance aquatique / semaine</li>
+                        <li><i class="fas fa-check"></i> 1 cours en salle* / mois </li>
+                        <li><i class="fas fa-check"></i> Espace détente 4 € </li>
+                    </ul>
+                    <div class='tarif_footer'>
+                        <div>* Yoga et Thai Chi compris</div>
+                        <div>** Séance aqua supplémentaire : 6€ </div>
+                        <div>** Cours en salle supplémentaire : 4€</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="box featured">
+                    <h3>ADAPT 2 - 12 mois **</h3>
+                    <h4>60<sup>€</sup><span> / mois</span></h4>
+                    <ul>
+                        <li><i class="fas fa-check"></i> 2 séances aquatique / semaine</li>
+                        <li><i class="fas fa-check"></i> 1 cours en salle* / semaine</li>
+                        <li><i class="fas fa-check"></i> Espace détente illimité</li>
+                    </ul>
+                    <div class='tarif_footer'>
+                        <div>* Yoga et Thai Chi compris</div>
+                        <div>** Séance aqua supplémentaire : 6€ </div>
+                        <div>** Cours en salle supplémentaire : 4€</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="box featured">
+                    <h3>ADAPT 3 - 12 mois</h3>
+                    <h4>75<sup>€</sup><span> / mois</span></h4>
+                    <ul>
+                        <li><i class="fas fa-check"></i> Tout illimité*</li>
+                    </ul>
+                    <div class='tarif_footer'>
+                        <div>* Hydromassage et professionnel de santé non inclus</div>
+                    </div>
+                </div>
+            </div>
+        </div>    
+        <div class="row">   
+            <div class="col-lg-4 col-md-6">
+                <div class="box featured">
+                    <h3>ADAPT 1 - 6 mois</h3>
+                    <h4>45<sup>€</sup><span> / mois</span></h4>
+                    <ul>
+                        <li><i class="fas fa-check"></i> 1 séance aquatique / semaine</li>
+                        <li><i class="fas fa-check"></i> 1 cours en salle* / mois </li>
+                        <li><i class="fas fa-check"></i> Espace détente 4 € </li>
+                    </ul>
+                    <div class='tarif_footer'>
+                        <div>* Yoga et Thai Chi compris</div>
+                        <div>** Séance aqua supplémentaire : 6€ </div>
+                        <div>** Cours en salle supplémentaire : 4€</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="box featured">
+                    <h3>ADAPT 2 - 6 mois</h3>
+                    <h4>70<sup>€</sup><span> / mois</span></h4>
+                    <ul>
+                        <li><i class="fas fa-check"></i> 2 séances aquatique / semaine</li>
+                        <li><i class="fas fa-check"></i> 1 cours en salle* / semaine</li>
+                        <li><i class="fas fa-check"></i> Espace détente illimité</li>
+                    </ul>
+                    <div class='tarif_footer'>
+                        <div>* Yoga et Thai Chi compris</div>
+                        <div>** Séance aqua supplémentaire : 6€ </div>
+                        <div>** Cours en salle supplémentaire : 4€</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="box featured">
+                    <h3>ADAPT 3 - 6 mois</h3>
+                    <h4>85<sup>€</sup><span> / mois</span></h4>
+                    <ul>
+                        <li><i class="fas fa-check"></i> Tout illimité*</li>
+                    </ul>
+                    <div class='tarif_footer'>
+                        <div>* Hydromassage et professionnel de santé non inclus</div>
+                    </div>
+                </div>
+            </div>
+        </div>    
     </div>
 </section>
