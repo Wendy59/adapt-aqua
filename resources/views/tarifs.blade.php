21194b368d6a697c3a6184704c4f8fe63e69dd90
Wendy <wendynorbert.toradiha@gmail.com>
2019-02-24 17:21:05 +0100

    ADD : Tarifs sans engagement

--- resources/views/tarifs.blade.php
+++ resources/views/tarifs.blade.php
@@ -1,7 +1,7 @@
 <section id="tarifs">
     <div class="container wow fadeIn">
         <div class="section-header">
-            <h3 class="section-title">Tarifs</h3>
+            <h3 class="section-title">Tarifs avec engagement</h3>
         </div>
         <div class="row">   
             <div class="col-lg-4 col-md-6">
@@ -52,7 +52,7 @@
         <div class="row">   
             <div class="col-lg-4 col-md-6">
                 <div class="box featured">
-                    <h3>ADAPT 1 - 6 mois</h3>
+                    <h3>ADAPT 1 - 6 mois **</h3>
                     <h4>45<sup>€</sup><span> / mois</span></h4>
                     <ul>
                         <li><i class="fas fa-check"></i> 1 séance aquatique / semaine</li>
@@ -68,7 +68,7 @@
             </div>
             <div class="col-lg-4 col-md-6">
                 <div class="box featured">
-                    <h3>ADAPT 2 - 6 mois</h3>
+                    <h3>ADAPT 2 - 6 mois **</h3>
                     <h4>70<sup>€</sup><span> / mois</span></h4>
                     <ul>
                         <li><i class="fas fa-check"></i> 2 séances aquatique / semaine</li>
@@ -95,5 +95,141 @@
                 </div>
             </div>
         </div>    
+    </div>
+    
+    <div class="container wow fadeIn">
+        <div class="section-header marb-35">
+            <h3 class="section-title">Tarifs avec engagement</h3>
+        </div>
+        <div class="row">
+            <div id="menu-wrapper">
+                <div class="cours_aqua menu-tarif">
+                    <span class="clearfix">
+                        <span class='menu-title'>1 séance - cours aquatique</span>
+                        <span style="left: 275px; right: 44px;" class="menu-line"></span>
+                        <span class="menu-price">15.00€</span>
+                    </span>
+                </div>
+                <div class="cours_natation menu-tarif">
+                    <span class="clearfix">
+                        <span class='menu-title'>1 séance - cours de natation / aquaphobie</span>
+                        <span style="left: 430px; right: 44px;" class="menu-line"></span>
+                        <span class="menu-price">18.00€</span>
+                    </span>
+                </div>
+
+                <div class="cours_aqua menu-tarif">
+                    <span class="clearfix">
+                        <span class='menu-title'>5 séances - cours aquatique</span>
+                        <span style="left: 285px; right: 44px;" class="menu-line"></span>
+                        <span class="menu-price">70.00€</span>
+                    </span>
+                </div>
+                <div class="cours_natation menu-tarif">
+                    <span class="clearfix">
+                        <span class='menu-title'>5 séances - cours de natation / aquaphobie</span>
+                        <span style="left: 438px; right: 44px;" class="menu-line"></span>
+                        <span class="menu-price">85.00€</span>
+                    </span>
+                </div>
+
+                <div class="cours_aqua menu-tarif">
+                    <span class="clearfix">
+                        <span class='menu-title'>10 séances - cours aquatique</span>
+                        <span style="left: 295px; right: 50px;" class="menu-line"></span>
+                        <span class="menu-price">120.00€</span>
+                    </span>
+                </div>
+                <div class="cours_natation menu-tarif">
+                    <span class="clearfix">
+                        <span class='menu-title'>10 séances - cours de natation / aquaphobie</span>
+                        <span style="left: 445px; right: 50px;" class="menu-line"></span>
+                        <span class="menu-price">140.00€</span>
+                    </span>
+                </div>
+
+                <div class="cours_collectif menu-tarif">
+                    <span class="clearfix">
+                        <span class='menu-title'>1 séance - cours collectifs</span>
+                        <span style="left: 295px; right: 50px;" class="menu-line"></span>
+                        <span class="menu-price">8.00€</span>
+                    </span>
+                </div>
+                <div class="espace_detente menu-tarif">
+                    <span class="clearfix">
+                        <span class='menu-title'>1 séance - espace détente</span>
+                        <span style="left: 260px; right: 50px;" class="menu-line"></span>
+                        <span class="menu-price">10.00€</span>
+                    </span>
+                </div>
+
+                <div class="cours_collectif menu-tarif">
+                    <span class="clearfix">
+                        <span class='menu-title'>5 séances - cours collectifs</span>
+                        <span style="left: 295px; right: 50px;" class="menu-line"></span>
+                        <span class="menu-price">35.00€</span>
+                    </span>
+                </div>
+                <div class="espace_detente menu-tarif">
+                    <span class="clearfix">
+                        <span class='menu-title'>5 séances - espace détente</span>
+                        <span style="left: 265px; right: 50px;" class="menu-line"></span>
+                        <span class="menu-price">42.00€</span>
+                    </span>
+                </div>
+
+                <div class="cours_collectif menu-tarif">
+                    <span class="clearfix">
+                        <span class='menu-title'>10 séances - cours collectifs</span>
+                        <span style="left: 295px; right: 50px;" class="menu-line"></span>
+                        <span class="menu-price">60.00€</span>
+                    </span>
+                </div>
+                <div class="espace_detente menu-tarif">
+                    <span class="clearfix">
+                        <span class='menu-title'>10 séances - espace détente</span>
+                        <span style="left: 280px; right: 50px;" class="menu-line"></span>
+                        <span class="menu-price">65.00€</span>
+                    </span>
+                </div>
+
+                <div class="hydromassage menu-tarif">
+                    <span class="clearfix">
+                        <span class='menu-title'>Hydromassage : 10 minutes</span>
+                        <span style="left: 270px; right: 50px;" class="menu-line"></span>
+                        <span class="menu-price">10.00€</span>
+                    </span>
+                </div>
+                <div class="hydromassage menu-tarif">
+                    <span class="clearfix">
+                        <span class='menu-title'>Hydromassage : 15 minutes</span>
+                        <span style="left: 270px; right: 50px;" class="menu-line"></span>
+                        <span class="menu-price">14.00€</span>
+                    </span>
+                </div>
+                <div class="hydromassage menu-tarif">
+                    <span class="clearfix">
+                        <span class='menu-title'>Hydromassage : 20 minutes</span>
+                        <span style="left: 270px; right: 50px;" class="menu-line"></span>
+                        <span class="menu-price">18.00€</span>
+                    </span>
+                </div>
+                <div class="hydromassage menu-tarif">
+                    <span class="clearfix">
+                        <span class='menu-title'>Hydromassage : 25 minutes</span>
+                        <span style="left: 270px; right: 50px;" class="menu-line"></span>
+                        <span class="menu-price">22.00€</span>
+                    </span>
+                </div>
+                <div class="hydromassage menu-tarif">
+                    <span class="clearfix">
+                        <span class='menu-title'>Hydromassage : 30 minutes</span>
+                        <span style="left: 270px; right: 50px;" class="menu-line"></span>
+                        <span class="menu-price">25.00€</span>
+                    </span>
+                </div>
+            </div>
+
+          </div>
     </div>
 </section>
